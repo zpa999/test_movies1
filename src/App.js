10d0b4d1f1745e2aead1c2b17a9dc14537f00e8c
nxzpa007 <zpa007@gmail.com>
2022-05-25 16:50:24 +0900

    2205251650

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
 import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMoives = async () => {
-    const movies = await axios.get(
-      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies: movies.data.data.movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMoives();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                title={movie.title}
-                year={movie.year}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-                rating={movie.rating}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
+  );
 }
+
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMoives = async () => {
+    const movies = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies: movies.data.data.movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMoives();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                title={movie.title}
+                year={movie.year}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+                rating={movie.rating}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
