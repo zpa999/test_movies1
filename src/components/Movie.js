ba4152094bf6cf213ab0492c2e2ec89f4e0fc817
nxzpa007 <zpa007@gmail.com>
2022-06-10 19:11:03 +0900

    2208281729

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from "react-router-dom";
 
-function Movie({ title, year, summary, poster, genres, rating }) {
+function Movie({ title, year, summary, poster, genres, rating, large_poster }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h1 className="movie__title">{title}</h1>
-        <h2>rating : {rating}</h2>
-        <ul className="movie__ul">
-          {genres.map((genre) => {
-            return <li className="movie__genre">{genre}</li>;
-          })}
-        </ul>
-        <h3 className="movie__year">{year}</h3>
-        <p className="moive__summary">{summary.slice(0, 180)}...</p>
-      </div>
+      <Link
+        to={{
+          pathname: "/movie-detail",
+          state: { title, year, summary, poster, genres, rating },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h1 className="movie__title">{title}</h1>
+
+          <h2>rating : {rating}</h2>
+          <ul className="movie__ul">
+            {genres.map((genre) => {
+              return <li className="movie__genre">{genre}</li>;
+            })}
+          </ul>
+          <h3 className="movie__year">{year}</h3>
+          <p className="moive__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/test.js
+++ src/test.js
@@ -9,8 +9,7 @@ import Navigation from "./components/Navigation";
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home}></Route>
+      <Navigation />|<Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
       <Route path="/movie-detail" component={Detail} />
     </BrowserRouter>
